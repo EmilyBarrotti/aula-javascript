14b901f04ca40861ad2ada4584b5840a93c64bae
EmilyBarrotti <emilylouback84@gmail.com>
2024-10-02 21:50:31 -0300

    media

--- aula.js
+++ aula.js
@@ -4,4 +4,4 @@ const frutas = ['maca', 'manga', 'morango',
 console.log("A fruta na posição 1 é",frutas[1])
 console.log("A fruta na posição 4 é",frutas[5])
 console.log("Meu array tem",frutas.length,"frutas")
-console.log("Ùltima fruta", frutas )
+console.log("Ùltima fruta", frutas[frutas.length - 1] )
