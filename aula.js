4db122d7c8ce79e82747cf84357eab5fd56a3820
EmilyBarrotti <emilylouback84@gmail.com>
2024-10-02 21:47:31 -0300

    media

--- aula.js
+++ aula.js
@@ -2,5 +2,6 @@ const frutas = ['maca', 'manga', 'morango',
 'melancia', 'maracuja', 'acerola'];
 
 console.log("A fruta na posição 1 é",frutas[1])
-console.log("A fruta na posição 4 é",frutas[1])
+console.log("A fruta na posição 4 é",frutas[5])
 console.log("Meu array tem",frutas.length,"frutas")
+console.log("Ùltima fruta", frutas )
---
+++ media.js
@@ -0,0 +1,13 @@
+const n1 = 6
+const n2 = 4
+const n3 = 9
+
+const media =(n1 + n2 + n3)/3;
+
+if (media >=6 ){
+    console.log('Aluno aprovado com média', media)
+}
+else {
+    console.log('Aluno reprovado com media', media)
+}
+
